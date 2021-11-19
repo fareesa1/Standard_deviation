dfa44ea40d937428f1e73c8813fc9faf21e06be5
fareesa_mas <30589399+fareesa1@users.noreply.github.com>
2021-11-19 18:26:19 +0530

    Update class1.py

--- graphs/class1.py
+++ graphs/class1.py
@@ -21,17 +21,4 @@ import plotly.express as px
 
 df = pd.read_csv("class1.csv")
 
-fig = px.scatter(df,    x="Student Number",
-                        y="Marks"
-            )
-fig.update_layout(shapes=[
-    dict(
-      type= 'line',
-      y0= mean, y1= mean,
-      x0= 0, x1= total_entries
-    )
-])
 
-fig.update_yaxes(rangemode="tozero")
-
-fig.show()
